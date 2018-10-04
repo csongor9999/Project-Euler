78019dea489f509dd933bc5798031cc7db181db5
Csongor Szalai <csongor.szalai14@gmail.com>
2018-10-04 16:37:11 +0200

    Update main.cpp

--- 4. Largest palindrome product/main.cpp
+++ 4. Largest palindrome product/main.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main () {
 
     int maxn = 0;
+    int limit = 1000;
 
-    for (int i=1;i<1000;i++) {
-        for (int j=i;j<1000;j++) {
+    for (int i=1;i<limit;i++) {
+        for (int j=i;j<limit;j++) {
             string temp = to_string(i*j);
             string reverseNum = "";
             for (int k=temp.length()-1;k>=0;k--) {
